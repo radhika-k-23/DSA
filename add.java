c3f257d2c06ebbb1c6465516117522d39d95c2f0
Radhika K <raykabk2005@gmail.com>
2026-02-28 11:18:41 +0530

    Add java file

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        System.out.println(add(a,b));
+    }
+
+    static int add(int a,int b){
+        return a+b;
+    }
+}
